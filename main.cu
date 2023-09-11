ba9be224e34d7346e81f5924d1d9f0ac1a5048b3
Wojciech Konior <konior.wojciech@gmail.com>
2023-09-11 11:56:33 +0200

    cuda vector forth approach

--- main.cu
+++ main.cu
@@ -12,9 +12,11 @@ int main()
     // output stream test
     // retrieving data from gpu test
 
-    gpu::int32 a = 10;
+    int32 i = 10;
+    gpu::int32 a = i;
     gpu::int32 b = 20;
     gpu::int32 c = a;
+
     gpu::vector<int32> vec;
     int d = b;
     cout << a << " " << b << " " << c << " " << d << endl;
