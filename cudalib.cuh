64cfebb5c43b47f7f06eeee4bbcce7ba5001e6b4
Wojciech Konior <konior.wojciech@gmail.com>
2023-09-11 09:59:33 +0200

    cuda vector first approach

--- cudalib.cuh
+++ cudalib.cuh
@@ -4,108 +4,116 @@
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 #include <iostream>
+
 using namespace std;
 
-
-namespace cu
+template <typename T>
+__global__ void cudaCopyVariableOnGPU(int *i_dest, int *i_src)
 {
-    template<typename T>
-    __global__ void cudaCopyVariableOnGPU(int* i_dest, int* i_src)
-    {
-        *i_dest = *i_src;
-    }
-
-    template<typename T>
-    class cuVar
-    {
-    protected:
-        T *i;
-
-    public:
-        cuVar()
-        {
-            cudaDeclare();
-            std::cout << i << " cint default constructor\n";
-        }
-        cuVar(T val)
-        {
-            cudaAssign(val);
-            std::cout << i << " " << val << " cint assign constructor\n";
-        }
-        cuVar(const cuVar<T> &val)
-        {
-            cudaCopy(val);
-            std::cout << i << " " << val.i <<" cint cpy constructor\n";
-        }
-        ~cuVar()
-        {
-            cudaClear();
-        }
-
-        void cudaClear()
-        {
-            if(i != nullptr)
-            {
-                cudaFree(i);
-                i = nullptr;
-            }
-        }
-
-        void cudaDeclare()
-        {
-            cudaClear();
-            cudaMalloc(&i, sizeof(T));
-        }
-
-        void cudaAssign(T val)
-        {
-            cudaDeclare();
-            cudaMemcpy(i, &val, sizeof(T), cudaMemcpyHostToDevice);
-            // cout << "cudaAssign : " << i << " " << val << endl;
-        }
-
-        void cudaReference(const cuVar<T> &val)
-        {
-            cudaClear();
-            i = val.i;
-        }
-
-        void cudaCopy(const cuVar<T> &val)
-        {
-            // cout << "cudaCopy: i = " << get() << ", val = " << val.get() << endl;
-            cudaDeclare();
-            cudaCopyVariableOnGPU<T><<<1,1>>>(i, val.i);
-        }
-
-        T get() const
-        {
-            T host_var;
-            cudaMemcpy(&host_var, i, sizeof(T), cudaMemcpyDeviceToHost);
-            // cout << "cudaGet : " << i << " " << host_var << endl;
-            return host_var;
-        }
-
-        operator T() const { return get(); }
-        // friend ostream& operator<<(ostream& os, const cuVar<T>& gpu_val);
-    };
-
-    using int8 = cuVar<char>;
-    using int16 = cuVar<short>;
-    using int32 = cuVar<int>;
-    using int64 = cuVar<long long>;
-    using uint8 = cuVar<unsigned char>;
-    using uint16 = cuVar<unsigned short>;
-    using uint32 = cuVar<unsigned int>;
-    using uint64 = cuVar<unsigned long long>;
-    using float32 = cuVar<float>;
-    using float64 = cuVar<double>;
-    using float128 = cuVar<long double>;
+    *i_dest = *i_src;
 }
 
-template<typename T>
-ostream& operator<<(ostream& os, const cu::cuVar<T>& gpu_val)
+template <typename T>
+class cuVar
+{
+protected:
+    T *i;
+
+public:
+    cuVar() { cudaDeclare(); }
+    cuVar(T val) { cudaAssign(val); }
+    cuVar(const cuVar<T> &val) { cudaCopy(val); }
+    ~cuVar() { cudaClear(); }
+
+    void cudaClear()
+    {
+        if (i != nullptr)
+        {
+            cudaFree(i);
+            i = nullptr;
+        }
+    }
+
+    void cudaDeclare()
+    {
+        cudaClear();
+        cudaMalloc(&i, sizeof(T));
+    }
+
+    void cudaAssign(T val)
+    {
+        cudaDeclare();
+        cudaMemcpy(i, &val, sizeof(T), cudaMemcpyHostToDevice);
+    }
+
+    void cudaCopy(const cuVar<T> &val)
+    {
+        cudaDeclare();
+        cudaCopyVariableOnGPU<T><<<1, 1>>>(i, val.i);
+    }
+
+    T get() const
+    {
+        T host_var;
+        cudaMemcpy(&host_var, i, sizeof(T), cudaMemcpyDeviceToHost);
+        return host_var;
+    }
+
+    operator T() const { return get(); }
+};
+
+typedef char int8;
+typedef short int16;
+typedef int int32;
+typedef long long int64;
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long long uint64;
+typedef float float32;
+typedef double float64;
+typedef long double float128;
+
+namespace cuda
+{
+    namespace gpu
+    {
+        typedef cuVar<char> int8;
+        typedef cuVar<short> int16;
+        typedef cuVar<int> int32;
+        typedef cuVar<long long> int64;
+        typedef cuVar<unsigned char> uint8;
+        typedef cuVar<unsigned short> uint16;
+        typedef cuVar<unsigned int> uint32;
+        typedef cuVar<unsigned long long> uint64;
+        typedef cuVar<float> float32;
+        typedef cuVar<double> float64;
+        typedef cuVar<long double> float128;
+
+        template <typename T>
+        class vector
+        {
+        protected:
+            T *i;
+
+        public:
+            vector(){};
+        };
+
+    }
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const cuVar<T> &gpu_val)
 {
     os << gpu_val.get();
+    return os;
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const cuda::gpu::vector<T> &gpu_val)
+{
+    // os << gpu_val.get();
     return os;
 }
 
--- main.cu
+++ main.cu
@@ -3,65 +3,7 @@
 
 using namespace std;
 
-
-class int32
-{
-public:
-    int32(){cudaDeclare();}
-    int32(int value){
-        cudaAssign(value);
-        cout << "assignment constructor\n";
-    }
-    ~int32(){ cudaClear(); }
-
-    void cudaClear()
-    {
-        if(i!=nullptr)
-        {
-            cudaFree(i);
-            i = nullptr;
-        }
-    }
-
-    void cudaDeclare()
-    {
-        cudaClear();
-        cudaMalloc(&i, sizeof(int));
-    }
-
-    void cudaAssign(int value)
-    {
-        // cudaClear();
-        // cudaMalloc(&i, sizeof(int));
-        cudaDeclare();
-        cudaMemcpy(i, &value, sizeof(int), cudaMemcpyHostToDevice);
-    }
-
-    int get()
-    {
-        int value;
-        cudaMemcpy(&value, i, sizeof(int), cudaMemcpyDeviceToHost);
-        return value;
-    }
-
-public:
-    int *i;
-};
-
-void assignTest(){
-    int a = 10;
-    int32 gpu_a = a;
-    int b = gpu_a.get();
-    if(a == b) cout << "[   OK   ]\n";
-    else  cout << "[ NOT OK ]";
-
-    int c = 20;
-    int32 gpu_b = c;
-    int d = gpu_b.get();
-    if(c == d) cout << "[   OK   ]\n";
-    else  cout << "[ NOT OK ]";
-}
-
+using namespace cuda;
 int main()
 {
 
@@ -70,9 +12,10 @@ int main()
     // output stream test
     // retrieving data from gpu test
 
-    cu::int32 a = 10;
-    cu::int32 b = 20;
-    cu::int32 c = a;
+    gpu::int32 a = 10;
+    gpu::int32 b = 20;
+    gpu::int32 c = a;
+    gpu::vector<int32> vec;
     int d = b;
     cout << a << " " << b << " " << c << " " << d << endl;
     return 0;
