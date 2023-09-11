1645afd5fe3c70760fbae7d66c31ce6cf35efd21
Wojciech Konior <konior.wojciech@gmail.com>
2023-09-11 17:44:13 +0200

    cuda vector - rearrangements

--- main.cu
+++ main.cu
@@ -1,5 +1,6 @@
 #include <iostream>
-#include "cudalib.cuh"
+#include "cudascalar.cuh"
+#include "cudavector.cuh"
 
 using namespace std;
 
@@ -10,15 +11,26 @@ int main()
     // assignTest();
     // copy test
     // output stream test
-    // retrieving data from gpu test
+    // retrieving _data from gpu test
 
     int32 i = 10;
     gpu::int32 a = i;
     gpu::int32 b = 20;
     gpu::int32 c = a;
+    gpu::int32 &e = a;
 
-    gpu::vector<int32> vec;
+    std::vector<int32> v = {1, 2, 3, 4};
+    gpu::vector<int32> vec8({1, 2, 3, 4});
+    gpu::vector<int32> vec9 = {3, 4, 3, 2};
+    gpu::vector<int32> vec(v);
+    gpu::vector<int32> vec2(vec);
+    gpu::vector<int32> vec3 = v;
+    gpu::vector<int32> vec4 = vec3;
+
+    std::vector<int32> vec5 = vec4;
+    gpu::vector<int32> vec6 = vec5;
     int d = b;
-    cout << a << " " << b << " " << c << " " << d << endl;
+    cout << a << " " << b << " " << c << " " << d << " " << e << endl;
+    cout << vec << " "<<vec2 << " " << vec3 << " " << vec4<< " " << vec6 << vec5<< endl;
     return 0;
 }
