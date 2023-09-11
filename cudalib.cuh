f27f87eb0445c140f7dd107f81cb7c052c7a7cc4
Wojciech Konior <konior.wojciech@gmail.com>
2023-09-11 10:21:14 +0200

    cuda vector second approach

--- cudalib.cuh
+++ cudalib.cuh
@@ -4,63 +4,9 @@
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 #include <iostream>
+#include <vector>
 
 using namespace std;
-
-template <typename T>
-__global__ void cudaCopyVariableOnGPU(int *i_dest, int *i_src)
-{
-    *i_dest = *i_src;
-}
-
-template <typename T>
-class cuVar
-{
-protected:
-    T *i;
-
-public:
-    cuVar() { cudaDeclare(); }
-    cuVar(T val) { cudaAssign(val); }
-    cuVar(const cuVar<T> &val) { cudaCopy(val); }
-    ~cuVar() { cudaClear(); }
-
-    void cudaClear()
-    {
-        if (i != nullptr)
-        {
-            cudaFree(i);
-            i = nullptr;
-        }
-    }
-
-    void cudaDeclare()
-    {
-        cudaClear();
-        cudaMalloc(&i, sizeof(T));
-    }
-
-    void cudaAssign(T val)
-    {
-        cudaDeclare();
-        cudaMemcpy(i, &val, sizeof(T), cudaMemcpyHostToDevice);
-    }
-
-    void cudaCopy(const cuVar<T> &val)
-    {
-        cudaDeclare();
-        cudaCopyVariableOnGPU<T><<<1, 1>>>(i, val.i);
-    }
-
-    T get() const
-    {
-        T host_var;
-        cudaMemcpy(&host_var, i, sizeof(T), cudaMemcpyDeviceToHost);
-        return host_var;
-    }
-
-    operator T() const { return get(); }
-};
 
 typedef char int8;
 typedef short int16;
@@ -78,6 +24,61 @@ namespace cuda
 {
     namespace gpu
     {
+        template <typename T>
+        __global__ void cudaCopyVariableOnGPU(int *i_dest, int *i_src)
+        {
+            *i_dest = *i_src;
+        }
+
+        template <typename T>
+        class cuVar
+        {
+        protected:
+            T *data;
+
+        public:
+            cuVar() { cudaDeclare(); }
+            cuVar(T val) { cudaAssign(val); }
+            cuVar(const cuVar<T> &val) { cudaCopy(val); }
+            ~cuVar() { cudaClear(); }
+
+            void cudaClear()
+            {
+                if (data != nullptr)
+                {
+                    cudaFree(data);
+                    data = nullptr;
+                }
+            }
+
+            void cudaDeclare()
+            {
+                cudaClear();
+                cudaMalloc(&data, sizeof(T));
+            }
+
+            void cudaAssign(T val)
+            {
+                cudaDeclare();
+                cudaMemcpy(data, &val, sizeof(T), cudaMemcpyHostToDevice);
+            }
+
+            void cudaCopy(const cuVar<T> &val)
+            {
+                cudaDeclare();
+                cudaCopyVariableOnGPU<T><<<1, 1>>>(data, val.data);
+            }
+
+            T get() const
+            {
+                T host_var;
+                cudaMemcpy(&host_var, data, sizeof(T), cudaMemcpyDeviceToHost);
+                return host_var;
+            }
+
+            operator T() const { return get(); }
+        };
+
         typedef cuVar<char> int8;
         typedef cuVar<short> int16;
         typedef cuVar<int> int32;
@@ -94,17 +95,56 @@ namespace cuda
         class vector
         {
         protected:
-            T *i;
+            T *data;
+            size_t size;
 
         public:
-            vector(){};
-        };
+            vector() { size = 0; }
+            vector(std::vector<T>& vec) { cudaAssign(vec); }
+            vector(const cuda::gpu::vector<T> &cuda_vec) { cudaCopy(cuda_vec); }
+            ~vector() { cudaClear(); }
 
+            void cudaClear()
+            {
+                if (data != nullptr)
+                {
+                    cudaFree(data);
+                    data = nullptr;
+                }
+            }
+
+            void cudaDeclare()
+            {
+                cudaClear();
+                cudaMalloc(&data, size*sizeof(T));
+            }
+
+            void cudaAssign(std::vector<T>& vec)
+            {
+                cudaDeclare();
+                cudaMemcpy(data, &vec[0], size*sizeof(T), cudaMemcpyHostToDevice);
+            }
+
+            void cudaCopy(const cuda::gpu::vector<T> &cuda_vec)
+            {
+                cudaDeclare();
+                // cudaCopyVariableOnGPU<T><<<1, 1>>>(data, cuda_vec.data);
+            }
+
+            T get() const
+            {
+                T host_var;
+                // cudaMemcpy(&host_var, data, size*sizeof(T), cudaMemcpyDeviceToHost);
+                return host_var;
+            }
+
+            operator T() const { return get(); }
+        };
     }
 }
 
 template <typename T>
-ostream &operator<<(ostream &os, const cuVar<T> &gpu_val)
+ostream &operator<<(ostream &os, const cuda::gpu::cuVar<T> &gpu_val)
 {
     os << gpu_val.get();
     return os;
